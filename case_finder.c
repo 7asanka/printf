4bc4de781dccb2a5ba6868c4e3cdb5c3cc20f722
7asanka <hassan2002_kimo@hotmail.com>
2024-07-27 12:34:09 -0700

    hot fix

--- case_finder.c
+++ case_finder.c
@@ -27,11 +27,11 @@ int case_finder(const char ch, int count, va_list args)
 
 			if (str == NULL)
 				str = "(null)";
-			while (*str != '\0')
+			while (*str)
 			{
-				write(1, &str, 1);
-				str++;
+				write(1, str, 1);
 				count++;
+				str++;
 			}
 			break;
 		}
@@ -43,10 +43,8 @@ int case_finder(const char ch, int count, va_list args)
 		}
 		default:
 		{
-			char c = va_arg(args, int);
-
 			write(1, "%", 1);
-			write(1, &c, 1);
+			write(1, &ch, 1);
 			count += 2;
 			break;
 		}
