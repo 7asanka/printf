de498e549507396aec0cf089f385940fed26201c
7asanka <hassan2002_kimo@hotmail.com>
2024-07-27 11:06:47 -0700

    case_finder adjust

--- case_finder.c
+++ case_finder.c
@@ -1,11 +1,10 @@
 #include "main.h"
 #include <unistd.h>
 #include <stdarg.h>
-
 /**
  * case_finder - finds mod case for _printf
  * @ch: character for the case
- * @arg: argument list
+ * @args: argument list
  * @count: count to increase for function printf
  *
  * Return: count
@@ -44,15 +43,12 @@ int case_finder(const char ch, int count, va_list args)
 			break;
 		}
 		default:
-		{
 			char c = va_arg(args, int);
 
 			write(1, "%", 1);
 			write(1, &c, 1);
 			count += 2;
 			break;
-		}
 	}
-
 	return (count);
 }
